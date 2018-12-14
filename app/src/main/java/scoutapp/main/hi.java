c8a5598c9fc3d315523a70a28dc2c7d7b68eb080
jisferjake <34079357+JisferJake@users.noreply.github.com>
2018-12-13 21:32:15 -0500

    Fundamentals

---
+++ app/src/main/java/scoutapp/main/hi.java
@@ -0,0 +1,21 @@
+package scoutapp.main;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class hi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hi);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(home.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
