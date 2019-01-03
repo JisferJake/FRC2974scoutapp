b4b8e59a32d9901da44aaabf1a35f30621652cfe
jisferjake <34079357+JisferJake@users.noreply.github.com>
2019-01-03 12:31:44 -0500

    New Functions
    added Booleans and number increment

--- app/src/main/java/scoutapp/main/home.java
+++ app/src/main/java/scoutapp/main/home.java
@@ -5,10 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class home extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    boolean testBool = false;
+    int incrementNumber = 0;
+    public static final String EXTRA_MESSAGE = "com.example.frcScoutApp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,9 +20,37 @@ public class home extends AppCompatActivity {
 
     public void sendMessage(View view){
         Intent intent = new Intent(this, hi.class);
-        EditText editText = (EditText) findViewById(R.id.editText2);
+        EditText editText = findViewById(R.id.editText2);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void incrementValue(View view){
+        incrementNumber++;
+        display(incrementNumber, testBool);
+    }
+
+    public void decrementValue(View view){
+        incrementNumber--;
+        display(incrementNumber, testBool);
+    }
+
+    public void setTrue(View view){
+        testBool = true;
+        display(incrementNumber, testBool);
+    }
+
+    public void setFalse(View view){
+        testBool = false;
+        display(incrementNumber, testBool);
+    }
+
+    public void display(int number, boolean test){
+        TextView numberIncrement = findViewById(R.id.numberIncrement);
+        TextView trueFalse = findViewById(R.id.boolDisplay);
+        numberIncrement.setText("" + number);
+        trueFalse.setText("" + test);
+
     }
 }
