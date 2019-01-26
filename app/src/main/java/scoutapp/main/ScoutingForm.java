2ff4e36deea5d9d7968838c2f90b928daad42c20
Ethan Neece <34079301+Ethanneece@users.noreply.github.com>
2019-01-26 11:02:14 -0500

    please sir please

--- app/src/main/java/scoutapp/main/ScoutingForm.java
+++ app/src/main/java/scoutapp/main/ScoutingForm.java
@@ -1,12 +1,14 @@
 package scoutapp.main;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -15,6 +17,7 @@ public class ScoutingForm extends AppCompatActivity {
 
     private Map<Integer, AtomicInteger> buttonsToAtomicInt;
     private Map<Integer, Integer> buttonsToTextView;
+    private Map<String, AtomicInteger> numbersToIdentifiers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +28,12 @@ public class ScoutingForm extends AppCompatActivity {
         int[] plusButtons = {R.id.plusButtonACSC, R.id.plusButtonACSH, R.id.plusButtonARH, R.id.plusButtonARC, R.id.plusButtonTCSC, R.id.plusButtonTCSH, R.id.plusButtonTRC, R.id.plusButtonTRH};
         int[] minusButtons = {R.id.minusButtonACSC, R.id.minusButtonACSH, R.id.minusButtonARH, R.id.minusButtonARC, R.id.minusButtonTCSC, R.id.minusButtonTCSH, R.id.minusButtonTRC, R.id.minusButtonTRH};
         int[] textview = {R.id.autoCSC, R.id.autoCSH, R.id.autoRH, R.id.autoRC, R.id.teleOPCSC, R.id.teleOPCSH, R.id.teleOPRC, R.id.teleOPRH};
-
+        String[] names = { "Auto Cargo Ship Cargo", "Auto Cargo Ship Hatches", "Auto Rocket Hatches", "Auto Rocket Cargo", "Teleop Cargo Ship Cargo", "Teleop Cargo Ship Hatches", "Teleop Rocket Cargo", "Teleop Rocket Hatches"};
         buttonsToAtomicInt = new HashMap<>();
         buttonsToTextView = new HashMap<>();
+        numbersToIdentifiers = new HashMap<>();
 
+        //Mapping Buttons to HashMap buttonsToAtomicInt and buttonsToTextView
         for (int i = 0; i < plusButtons.length; i++) {
             AtomicInteger inty = new AtomicInteger(0);
             buttonsToAtomicInt.put(plusButtons[i], inty);
@@ -36,6 +41,8 @@ public class ScoutingForm extends AppCompatActivity {
 
             buttonsToTextView.put(plusButtons[i], textview[i]);
             buttonsToTextView.put(minusButtons[i], textview[i]);
+
+            numbersToIdentifiers.put(names[i], inty);
         }
 
         //Drop Menu of Alliance Colors
@@ -44,17 +51,19 @@ public class ScoutingForm extends AppCompatActivity {
         ArrayAdapter<String> dropColors = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, alliances);
         dropColor.setAdapter(dropColors);
 
-        //Drop Menu of
+        //Drop Menu of of Alliance Positions
         Spinner dropPos = findViewById(R.id.dropPosition);
         String[] positions = {"Position", "Close", "Middle", "Far"};
         ArrayAdapter<String> dropPositions = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, positions);
         dropPos.setAdapter(dropPositions);
 
+        //Drop Menu of Connections During Match
         Spinner dropConnection = findViewById(R.id.dropConnection);
         String[] connections = {"None", "Once", "More than once"};
         ArrayAdapter<String> dropConnections = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, connections);
         dropConnection.setAdapter(dropConnections);
 
+        //Drop Menus of levels climbed to
         Spinner dropClimb = findViewById(R.id.dropClimb);
         String[] climbs = {"Level Climbed to", "Garbage", "Level 1", "Level 2", "Level 3"};
         ArrayAdapter<String> dropClimbs = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, climbs);
@@ -82,5 +91,21 @@ public class ScoutingForm extends AppCompatActivity {
             TextView text = findViewById(buttonsToTextView.get(view.getId()));
             text.setText(buttonsToAtomicInt.get(view.getId()).toString());
         }
+    }
+
+    public void export(View view) {
+        final String DIRECTORY_2974SCOUTINGAPP = "2974ScoutingApp";         //Direction the Scouting App Folder is on the Android.
+        System.out.println(Environment.DIRECTORY_DOWNLOADS);
+        File file = new File(Environment.getExternalStoragePublicDirectory(DIRECTORY_2974SCOUTINGAPP), "ScoutingForm" + System.currentTimeMillis() + ".csv");
+        try {
+            file.createNewFile();
+            PrintWriter writer = new PrintWriter(file);
+            for(String key : numbersToIdentifiers.keySet())
+                writer.write(key + " " + numbersToIdentifiers.get(key) + "\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Hello");
     }
 }
