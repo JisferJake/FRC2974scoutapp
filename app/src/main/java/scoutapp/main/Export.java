0de0833b7ad169c25750e8507a354813654f2b6d
Ethan Neece <34079301+Ethanneece@users.noreply.github.com>
2019-02-18 16:52:23 -0700

    progress

--- app/src/main/java/scoutapp/main/Export.java
+++ app/src/main/java/scoutapp/main/Export.java
@@ -2,65 +2,130 @@ package scoutapp.main;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
 public class Export extends AppCompatActivity {
 
-    private String pairedDevice;
+    private static final String URI = "uri";
+    private static final String DESTINATION = "AC-FD-CE-30-1C-23";
+    private static final String correctDevice = "DESKTOP-DDTDG6C";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_export);
 
-        pairedDevice = "DESKTOP-DDTDG6G";
     }
 
     public void exportScoutingForm(View view) {
+        setBlueTooth();
+
         final String DIRECTORY_2974SCOUTINGAPP = "2974ScoutingApp";
         File folder = Environment.getExternalStoragePublicDirectory(DIRECTORY_2974SCOUTINGAPP);
         File[] files = folder.listFiles();
-        for(File file : files)
-            if (file.getName().contains("ScoutingForm")) {
-                BluetoothDevice device = setBlueTooth();
-                try {
-                    BluetoothSocket socket = device.createRfcommSocketToServiceRecord(UUID.randomUUID());
-                    socket.connect();
-                    FileInputStream input = new FileInputStream(file);
-                    byte[] fileContent = new byte[(int) file.length()];
-                    input.read(fileContent);
-                    socket.getOutputStream().write(fileContent);
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+
     }
 
+
     private BluetoothDevice setBlueTooth() {
-        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-        while(!adapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, 1);
-            adapter = BluetoothAdapter.getDefaultAdapter();
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter == null)
+            System.out.println("Bluetooth not supported");
+
+        if(!bluetoothAdapter.isEnabled()) {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 1);
         }
 
-        for(BluetoothDevice device : adapter.getBondedDevices())
-            if (device.getName().equals(pairedDevice))
-                return device;
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
 
-        return null;
+        BluetoothDevice foundDevice = null;
+        for(BluetoothDevice device : pairedDevices) {
+            if(correctDevice.equals(device.getName())) {
+                foundDevice = device;
+            }
+        }
+
+        if(foundDevice == null) {
+            Intent discoverable = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverable.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverable);
+        }
+
+        while(foundDevice == null);
+
+        ConnectThread connect = new ConnectThread(foundDevice);
+
+        connect.run();
+
+        return foundDevice;
     }
 
     public void exportPitForm() {
+    }
 
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket
+            // because mmSocket is final.
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            try {
+                // Get a BluetoothSocket to connect with the given BluetoothDevice.
+                // MY_UUID is the app's UUID string, also used in the server code.
+                tmp = device.createRfcommSocketToServiceRecord(UUID.randomUUID());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it otherwise slows down the connection.
+
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                    closeException.printStackTrace();
+                }
+                return;
+            }
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
